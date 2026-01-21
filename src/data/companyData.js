ac7b649de431a51a4f38d8baf8f95bb101d14c95
Vincent Stute <vincent.stute@gmx.de>
2026-01-21 12:07:06 +0100

    mock company data

--- src/data/companyData.js
+++ src/data/companyData.js
@@ -17,6 +17,11 @@ const companies = [
     riskCategory: "Money Laundering risk",
     riskLevel: 3,
     date: "18.1.2026",
+    reasoning:
+      "Adverse media reveals with recent issues of the business partner with regard to tax. This is an indication that the business partner poses a certain risk to KION to become involved in tax evasion schemes and possibly other topics. The topic is considered a high risk topic, however the probability of KION being negatively affected by this is consideredn relatively low.",
+    summary:
+      "French tax authorities have demanded over €550 million from Coca-Cola Europacific Partners France (CCEP) after audits covering 2014–2019, according to its 2022 financial filings. CCEP was asked to pay €370 million for 2014–2015, €182.2 million for 2016, and may face additional adjustments for 2018 and 2019. The dispute mainly concerns the pricing terms for concentrate purchased from The Coca-Cola Company in the U.S. CCEP says it has strong grounds to contest the claims and has made no provisions. Another subsidiary, Coca-Cola Services France, is also under audit and reportedly faces at least €140 million in adjustments. CCEP reported more than €2 billion in French revenue for 2022 and employs over 2,500 people.",
+    link: "https://www.tagesschau.de/ausland/amerika/usa-oeltanker-102.html",
   },
   {
     id: 3,
@@ -24,6 +29,11 @@ const companies = [
     riskCategory: "Child Labor risk",
     riskLevel: 2,
     date: "20.12.2025",
+    reasoning:
+      "Adverse media reveals with recent issues of the business partner with regard to tax. This is an indication that the business partner poses a certain risk to KION to become involved in tax evasion schemes and possibly other topics. The topic is considered a high risk topic, however the probability of KION being negatively affected by this is consideredn relatively low.",
+    summary:
+      "French tax authorities have demanded over €550 million from Coca-Cola Europacific Partners France (CCEP) after audits covering 2014–2019, according to its 2022 financial filings. CCEP was asked to pay €370 million for 2014–2015, €182.2 million for 2016, and may face additional adjustments for 2018 and 2019. The dispute mainly concerns the pricing terms for concentrate purchased from The Coca-Cola Company in the U.S. CCEP says it has strong grounds to contest the claims and has made no provisions. Another subsidiary, Coca-Cola Services France, is also under audit and reportedly faces at least €140 million in adjustments. CCEP reported more than €2 billion in French revenue for 2022 and employs over 2,500 people.",
+    link: "https://www.tagesschau.de/ausland/amerika/usa-oeltanker-102.html",
   },
   {
     id: 4,
@@ -31,6 +41,11 @@ const companies = [
     riskCategory: "Child Labor risk",
     riskLevel: 2,
     date: "20.12.2025",
+    reasoning:
+      "Adverse media reveals with recent issues of the business partner with regard to tax. This is an indication that the business partner poses a certain risk to KION to become involved in tax evasion schemes and possibly other topics. The topic is considered a high risk topic, however the probability of KION being negatively affected by this is consideredn relatively low.",
+    summary:
+      "French tax authorities have demanded over €550 million from Coca-Cola Europacific Partners France (CCEP) after audits covering 2014–2019, according to its 2022 financial filings. CCEP was asked to pay €370 million for 2014–2015, €182.2 million for 2016, and may face additional adjustments for 2018 and 2019. The dispute mainly concerns the pricing terms for concentrate purchased from The Coca-Cola Company in the U.S. CCEP says it has strong grounds to contest the claims and has made no provisions. Another subsidiary, Coca-Cola Services France, is also under audit and reportedly faces at least €140 million in adjustments. CCEP reported more than €2 billion in French revenue for 2022 and employs over 2,500 people.",
+    link: "https://www.tagesschau.de/ausland/amerika/usa-oeltanker-102.html",
   },
   {
     id: 5,
@@ -38,6 +53,11 @@ const companies = [
     riskCategory: "Money Laundering risk",
     riskLevel: 1,
     date: "18.1.2026",
+    reasoning:
+      "Adverse media reveals with recent issues of the business partner with regard to tax. This is an indication that the business partner poses a certain risk to KION to become involved in tax evasion schemes and possibly other topics. The topic is considered a high risk topic, however the probability of KION being negatively affected by this is consideredn relatively low.",
+    summary:
+      "French tax authorities have demanded over €550 million from Coca-Cola Europacific Partners France (CCEP) after audits covering 2014–2019, according to its 2022 financial filings. CCEP was asked to pay €370 million for 2014–2015, €182.2 million for 2016, and may face additional adjustments for 2018 and 2019. The dispute mainly concerns the pricing terms for concentrate purchased from The Coca-Cola Company in the U.S. CCEP says it has strong grounds to contest the claims and has made no provisions. Another subsidiary, Coca-Cola Services France, is also under audit and reportedly faces at least €140 million in adjustments. CCEP reported more than €2 billion in French revenue for 2022 and employs over 2,500 people.",
+    link: "https://www.tagesschau.de/ausland/amerika/usa-oeltanker-102.html",
   },
 ];
 
